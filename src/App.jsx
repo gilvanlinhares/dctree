9023902c4c5c6f579fa7bc10cb2bee8a0ebeec19
GILVAN LINHARES DE SOUSA <GILVAN0360@DIGITALCOLLEGE.LOCAL>
2024-03-16 16:58:42 -0300

    Renderizando componente dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import { useState } from 'react';
+import './App.css';
+
+import Link from './components/Link';
+import LinkList from './components/LinkList';
+
+const links = [
+  {
+    label: "Quero aprender a programar"
+  },
+  {
+    label: "Quero aprender data analytics"
+  },
+  {
+    label: "Quero aprender marketing digital"
+  },
+  {
+    label: "Increva-se nos nossos cursos",
+    title: "Curso intensivo",
+    description: "Veja aqui quais os cursos intensivos estão com vagas abertas e garanta já a sua."
+  },
+  {
+    label: "Clique e se inscreva",
+    title: "Digital Plus",
+    description: "Evento exclusivo para alunos!"
+  }
+];
+
 
 function App() {
-  return (<div>Hello World</div>);
+  return (
+  <LinkList>
+    { links.map(item => <Link {...item} />)}
+  </LinkList>
+  );
 }
 
 export default App;
