d10710154b86b3e9135763bda7e2354160750727
GILVAN LINHARES DE SOUSA <GILVAN0360@DIGITALCOLLEGE.LOCAL>
2024-03-23 16:49:39 -0300

    Renomeando componentes

---
+++ src/components/LinkItem/index.jsx
@@ -0,0 +1,17 @@
+import './LinkItem.css';
+
+export default function LinkItem({title, description, label}) {
+    return (
+        <div className="link-wrap">
+
+            {title && description && (
+                    <>
+                        <span className="title">{title}</span>
+                        <span className="description">{description}</span>
+                    </>
+            )}
+
+        <a href="#" className="link-item">{ label }</a>
+        </div>
+    );
+}
